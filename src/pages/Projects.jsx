17acc06a27bfc4ee14ac3692d28b330438b07805
posaidon09 <halohalo090909@gmail.com>
2025-01-18 21:28:06 +0100

    style

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,11 @@ export default function Projects() {
 	return (
 		<div className="flex flex-col items-center min-h-screen mb-10">
 			<h1 className="text-text-50 font-bold text-4xl mt-32">My projects</h1>
-			<div className="flex flex-col xl:flex-row items-center justify-center gap-10 mt-24">
+			<div className="group flex flex-col xl:flex-row items-center justify-center mt-24">
 				<a
 					href="https://github.com/posaidon09/ytdt"
 					target="_blank"
-					className="w-[300px] h-[350px] rounded-xl p-3 ring ring-red-500 bg-gradient-to-br bg-black from-black from-30% to-red-500/20 to-90% hover:bg-red-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
+					className="w-[300px] h-[350px] rounded-xl z-20 hover:z-40 -rotate-12 p-3 ring ring-red-500 bg-gradient-to-br bg-black from-black from-30% to-red-500/20 to-90% hover:bg-red-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
 				>
 					<img
 						src="https://i.postimg.cc/vmhZCmZZ/Yoube.png"
@@ -22,7 +22,7 @@ export default function Projects() {
 				<a
 					href="https://github.com/posaidon09/spod"
 					target="_blank"
-					className="w-[300px] h-[350px] rounded-xl p-3 ring ring-green-500 bg-gradient-to-br bg-black from-black from-30% to-green-500/20 to-90% hover:bg-green-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
+					className="w-[300px] h-[350px] rounded-xl z-10 hover:z-40 group-hover:z-30 p-3 ring -ml-48 ring-green-500 bg-gradient-to-br bg-black from-black from-30% to-green-500/20 to-90% hover:bg-green-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
 				>
 					<img
 						src="https://i.postimg.cc/fyJSC7gs/image-psd.png"
@@ -38,7 +38,7 @@ export default function Projects() {
 				<a
 					href="https://github.com/posaidon09/Rtemp"
 					target="_blank"
-					className="w-[300px] h-[350px] rounded-xl p-3 ring ring-blue-500 bg-gradient-to-br bg-black from-black from-30% to-blue-500/20 to-90% hover:bg-blue-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
+					className="w-[300px] h-[350px] rounded-xl z-0 hover:z-40 p-3 rotate-12 -ml-48 ring ring-blue-500 bg-gradient-to-br bg-black =-black from-30% to-blue-500/20 to-90% hover:bg-blue-600/25 cursor-pointer hover:scale-110 transition-all duration-200"
 				>
 					<img
 						src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -4,7 +4,7 @@ export default function Root() {
 			<div className="flex flex-col gap-4 items-center justify-center -mt-4">
 				<img
 					src="https://i.postimg.cc/mkjf4GVr/Posaidon-as-a-cat-scaled-12x-pngcrushed.png"
-					className="scale-75 p-5 bg-gradient-to-br from-slate-900 to-gray-900/40 rounded-full"
+					className="scale-75 p-5 bg-gradient-to-br from-slate-900 to-gray-900/40 border-[6px] border-primary-700 rounded-full"
 				/>
 				<h1 className="text-text-50 text-5xl text-center font-extrabold -mt-5">
 					Posaidon
